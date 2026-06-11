7aa126099764df7408423ed9dd3e6d1bbdfc9883
24cse-deepika <deepika.bt24cse@pec.edu.in>
2026-06-11 23:23:05 +0530

    initial project setup with sidebar navigation

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
